0e44e2f372edfa0172dc0d447ba8fda73e3ff2cd
FormApi <siddeswarapradeep@gmail.com>
2023-09-27 18:45:49 +0530

    update

--- Controller/LoginProfile_Controller.js
+++ Controller/LoginProfile_Controller.js
@@ -1,27 +1,37 @@
 const LoginProfile = require("../Model/LoginProfile_Model")
 
 
-exports.create = async(req, res) => {
-    console.log(req.protocol + "://" + req.get("host"), "url")
-    if(!req.body) {
-        res.status(400).send("Content Connt Be Empty")
-        return
+exports.create = async (req, res) => {
+    if (!req.body) {
+        res.status(400).send("Content Cannot Be Empty");
+        return;
     }
-    const newLoginProfile = new  LoginProfile({
-        image: req.file&&req.file.filename ? req.protocol + "://" +req.get("host")+"/images/" + req.file.filename : "",
+
+    // Check if the email already exists in the database
+    const existingProfile = await LoginProfile.findOne({ email: req.body.email });
+
+    if (existingProfile) {
+        // If the email already exists, return a 409 Conflict response
+        return res.status(409).json({ message: "Email already exists" });
+    }
+
+    // If the email is unique, create a new LoginProfile
+    const newLoginProfile = new LoginProfile({
+        image: req.body.image,
         name: req.body.name,
         email: req.body.email,
+    });
 
-    })
-    newLoginProfile.save(newLoginProfile)
-                   .then(data => {
-                    res.status(200).json({ message: "success", Data: newLoginProfile});
-                })
-                   .catch(error => {
-                    res.status(500).json({ message: "Internal server error", error });
-                   })
+    newLoginProfile
+        .save()
+        .then((data) => {
+            res.status(201).json({ message: "Successfully Logged In", Data: newLoginProfile });
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Internal server error", error });
+        });
+};
 
-                }
 
 
             //    GET all specialists
